467b7a9f83b44d31dbd7e1a9d221e37bd4c0c858
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-09 22:00:01 +0200

    form testing-extra credit-2

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,20 +5,30 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
+
+const buildLoginForm = () => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  const username = screen.getByLabelText(/username/i)
-  const password = screen.getByLabelText(/password/i)
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
 
-  userEvent.type(username, 'someusername')
-  userEvent.type(password, 'somepassword')
+  const {username, password} = buildLoginForm()
+
+  userEvent.type(usernameInput, username)
+  userEvent.type(passwordInput, password)
 
   const button = screen.getByRole('button', {name: /submit/i})
   userEvent.click(button)
   expect(handleSubmit).toBeCalledWith({
-    username: 'someusername',
-    password: 'somepassword',
+    username,
+    password,
   })
 })
