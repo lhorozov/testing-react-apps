aa09c05a899646be8b88c1d485606335ebd208ff
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-09 22:14:10 +0200

    form testing-extra credit-4

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -3,15 +3,15 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
-import faker from 'faker'
 
-const buildLoginForm = ({username, password} = {}) => {
-  return {
-    username: username ?? faker.internet.userName(),
-    password: password ?? faker.internet.password(),
-  }
-}
+const buildLoginForm = build({
+  fields: {
+    username: fake(faker => faker.internet.userName()),
+    password: fake(faker => faker.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
@@ -19,7 +19,7 @@ test('submitting the form calls onSubmit with username and password', () => {
   const usernameInput = screen.getByLabelText(/username/i)
   const passwordInput = screen.getByLabelText(/password/i)
 
-  const {username, password} = buildLoginForm({password: '123'})
+  const {username, password} = buildLoginForm()
 
   userEvent.type(usernameInput, username)
   userEvent.type(passwordInput, password)
