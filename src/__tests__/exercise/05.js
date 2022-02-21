bb00f029fc63807b359092a8a049b8a5ca5269a4
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-21 21:32:45 +0200

    mocking HTTP requests-extra credit-2

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -6,7 +6,6 @@ import {waitForElementToBeRemoved} from '@testing-library/react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 
 import Login from '../../components/login-submission'
@@ -37,4 +36,18 @@ test(`logging in displays the user's username`, async () => {
   await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
 
   expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test(`add username required message if username is missing`, async () => {
+  render(<Login />)
+  const {password} = buildLoginForm()
+
+  // userEvent.type(screen.getByLabelText(/username/i), username)
+  userEvent.type(screen.getByLabelText(/password/i), password)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(screen.getByLabelText(/loading/i))
+
+  expect(screen.getByText(/username required/i)).toBeInTheDocument()
 })
