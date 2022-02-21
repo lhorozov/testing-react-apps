18da001f2fa5a76c397cfd74dbe2e1ce703fa2eb
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-21 22:11:22 +0200

    mocking HTTP requests-extra credit-4

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -10,6 +10,7 @@ import {setupServer} from 'msw/node'
 
 import Login from '../../components/login-submission'
 import {handlers} from '../../test/server-handlers'
+import {rest} from 'msw'
 
 const buildLoginForm = build({
   fields: {
@@ -23,6 +24,7 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -52,4 +54,25 @@ test(`add username required message if username is missing`, async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"username required"`,
   )
+})
+
+test(`unknown server error displays the error message`, async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
