d0ef4ceaeae6e17f13a7877fe0dae2f0c0e04dcc
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-24 21:07:21 +0200

    testing with context and a custom render method-extra credit

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -7,14 +7,27 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  render(
-    <ThemeProvider>
-      <EasyButton>Easy</EasyButton>
-    </ThemeProvider>,
+  const LightTheme = ({children}) => (
+    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
   )
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: LightTheme})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
+  `)
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  const DarkTheme = ({children}) => (
+    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  )
+
+  render(<EasyButton>Easy</EasyButton>, {wrapper: DarkTheme})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
   `)
 })
