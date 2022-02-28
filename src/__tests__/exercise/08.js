761089660f89c4293fd1aaf58d5045aeff7020b6
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-28 21:41:40 +0200

    testing custom hooks-extra credit-3

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,55 +1,46 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
-
-const setup = ({initialProps} = {}) => {
-  let result = {current: {}}
-  function TestComponent() {
-    Object.assign(result.current, useCounter(initialProps))
-    return null
-  }
-  render(<TestComponent />)
-  return result.current
-}
+import {renderHook, act} from '@testing-library/react-hooks'
 
 test('exposes the count and increment/decrement functions', () => {
-  const result = setup()
-  expect(result.count).toBe(0)
+  const {result} = renderHook(useCounter)
+  expect(result.current.count).toBe(0)
   act(() => {
-    result.increment()
+    result.current.increment()
   })
-  expect(result.count).toBe(1)
+  expect(result.current.count).toBe(1)
   act(() => {
-    result.decrement()
+    result.current.decrement()
   })
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 })
 
 test('allows customization of the initial count', () => {
-  const result = setup({initialProps: {initialCount: 5}})
-  expect(result.count).toBe(5)
+  const {result} = renderHook(() => useCounter({initialCount: 5}))
+
+  expect(result.current.count).toBe(5)
   act(() => {
-    result.increment()
+    result.current.increment()
   })
-  expect(result.count).toBe(6)
+  expect(result.current.count).toBe(6)
   act(() => {
-    result.decrement()
+    result.current.decrement()
   })
-  expect(result.count).toBe(5)
+  expect(result.current.count).toBe(5)
 })
 
 test('allows customization of the initial step', () => {
-  const result = setup({initialProps: {step: 5}})
-  expect(result.count).toBe(0)
+  const {result} = renderHook(() => useCounter({step: 5}))
+
+  expect(result.current.count).toBe(0)
   act(() => {
-    result.increment()
+    result.current.increment()
   })
-  expect(result.count).toBe(5)
+  expect(result.current.count).toBe(5)
   act(() => {
-    result.decrement()
+    result.current.decrement()
   })
-  expect(result.count).toBe(0)
+  expect(result.current.count).toBe(0)
 })
