6bb7ac72807e71f2ed1fd6823525fd404e4e6156
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-02-28 21:00:36 +0200

    testing custom hooks-extra credit

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,38 +2,29 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {fireEvent, render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-const TestUseCounter = () => {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <>
-      <div data-testid="count">{count}</div>
-      <button onClick={increment} data-testid="increment">
-        increment
-      </button>
-      <button onClick={decrement} data-testid="decrement">
-        decrement
-      </button>
-    </>
-  )
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  render(<TestUseCounter />)
-  const count = screen.getByTestId('count')
-  const incrementButton = screen.getByTestId('increment')
-  const decrementButton = screen.getByTestId('decrement')
-  expect(count).toHaveTextContent(0)
-  fireEvent.click(incrementButton)
-  fireEvent.click(incrementButton)
-  expect(count).toHaveTextContent(2)
-  fireEvent.click(decrementButton)
-  fireEvent.click(decrementButton)
-  fireEvent.click(decrementButton)
-  expect(count).toHaveTextContent(-1)
-})
+  let result
 
-/* eslint no-unused-vars:0 */
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+
+  act(() => {
+    result.increment()
+  })
+
+  expect(result.count).toBe(1)
+
+  act(() => {
+    result.decrement()
+  })
+
+  expect(result.count).toBe(0)
+})
